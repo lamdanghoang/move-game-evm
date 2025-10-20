aad075f148c7713df0c03c19713cad83d366865d
lamdanghoang <danghlambk14@gmail.com>
2025-10-20 15:55:12 +0700

    update socket client

--- lib/socketClient.ts
+++ lib/socketClient.ts
@@ -2,4 +2,6 @@
 
 import { io } from "socket.io-client";
 
-export const socket = io("http://localhost:3000");
+const URL = process.env.NEXT_PUBLIC_SOCKET_URL || "http://localhost:3000";
+
+export const socket = io(URL);
